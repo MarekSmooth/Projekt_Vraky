362e0f3ed00e32404b2d1d77de2bec568b17c321
MarekSmooth <hladky.mary@gmail.com>
2025-10-07 11:39:34 +0200

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        kontakt: resolve(__dirname, 'kontakt.html'),
+        onas: resolve(__dirname, 'onas.html'),
+        sluzby: resolve(__dirname, 'sluzby.html'),
+        cookies: resolve(__dirname, 'cookies.html'),
+        gdpr: resolve(__dirname, 'gdpr.html'),
+        vop: resolve(__dirname, 'vop.html'),
+      }
+    }
+  }
 })
